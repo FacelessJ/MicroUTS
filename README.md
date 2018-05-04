619dec70c24bcfc4da4e54c1669b8e8d7132771f
FacelessJ <bitbucket@facelessj.com>
2018-05-04 10:50:33 +1000

    Add simple Experimenter workflow

--- README.md
+++ README.md
@@ -2,14 +2,32 @@
 
 Scaffolding for MicroRTS AI development. This repository will be used for developing an AI and testing it against MicroRTS built-in AI as well as other open source agents.
 
-## Usage
+## Installation
 
 Make sure to include the `microrts.jar` library located in `lib` for access to MicroRTS.
 
-## Third Party AI
+### Third Party AI
 
 This repo includes a few third party AI for comparison and training:
+
 - [MicroRTS](https://github.com/nbarriga/microRTSbot)
 - [SCV](https://github.com/rubensolv/SCV)
 
 The compiled `.jar` files for each can be found in the `lib` directory
+
+## Usage
+
+The primary goal of this scaffolding is to easily run AI simulations from the command line using MicroRTS. Currently this uses simple unnamed command line arguments, however this will be improved later to use configuration files.
+
+The current format for the command line arguments is:
+
+`microuts.jar botfile.txt mapsfile.txt resultsfile.txt iterations (specificAI) (traceDir)`
+
+where:
+
+- `botfile.txt` is a named list of bots to test, separated by new lines. Every permutation (not combination) of these bots are simulated: that is, Bot 0 vs Bot 1 and Bot 1 vs Bot 0 are simulated.
+- `mapsfile.txt` is a list of map filenames to use in testing, separated by new lines
+- `resultsfile.txt` is the output location of the tests
+- `iterations` is an integer argument specifying how many times each match should be repeated
+- `(specificAI)` is an optional integer argument to restrict simulations to only the chosen AI. i.e., `specificAI=0` results in only the matches involving the first bot in the `botfile.txt` will be simulated
+- `(traceDir)` is an optional output directory to save traces to, which can be viewed by the MicroRTS GUI frontend
--- src/MicroUTS.java
+++ src/MicroUTS.java
@@ -4,21 +4,116 @@ import ai.core.AI;
 import rts.units.UnitTypeTable;
 import standard.StrategyTactics;
 import ai.pvai.PVAIML_ED;
+import tests.Experimenter;
+import tests.RunConfigurableExperiments;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class MicroUTS {
     static UnitTypeTable utt = new UnitTypeTable(UnitTypeTable.VERSION_ORIGINAL_FINETUNED, UnitTypeTable.MOVE_CONFLICT_RESOLUTION_CANCEL_BOTH);
+    private static List<AI> bots = new LinkedList<AI>();
+    private static List<PhysicalGameState> maps = new LinkedList<PhysicalGameState>();
+
+    public static AI getBotExtended(String botName) {
+        switch (botName) {
+            case "StrategyTactics":
+                try {
+                    return new StrategyTactics(utt);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            case "PVAIML_ED":
+                return new PVAIML_ED(utt);
+            /** Todo: Add socket AI */
+            default:
+                return RunConfigurableExperiments.getBot(botName);
+        }
+    }
+
+    public static void loadBots(String botFileName, List<AI> botList) throws IOException {
+        try (Stream<String> lines = Files.lines(Paths.get(botFileName), Charset.defaultCharset())) {
+            lines.forEachOrdered(line -> {
+                try {
+                    if (!line.startsWith("#") && !line.isEmpty()) {
+                        botList.add(getBotExtended(line));
+                    }
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
+    }
+
+    public static void loadMaps(String mapFileName, List<PhysicalGameState> mapList) throws IOException {
+        if (mapFileName.endsWith(".txt")) {
+            try (Stream<String> lines = Files.lines(Paths.get(mapFileName), Charset.defaultCharset())) {
+                lines.forEachOrdered(line -> {
+                    try {
+                        if (!line.startsWith("#") && !line.isEmpty()) {
+                            mapList.add(PhysicalGameState.load(line, utt));
+                        }
+                    } catch (Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+            }
+        } else if (mapFileName.endsWith(".xml")) {
+            try {
+                mapList.add(PhysicalGameState.load(mapFileName, utt));
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        } else {
+            throw new IllegalArgumentException("Map file name must end in .txt "
+                    + "(for a list of maps) or .xml (for a single map).");
+        }
+    }
+
+    /**
+     * Runs bot tests from command line
+     * @param args - Command line arguments: botfile mapsfile resultsfile iterations (specificAI) (traceDir)
+     *             where (specificAI) tells the experimenter to ignore all runs not involving the chosen AI
+     *             (indexed according to botfile). Set to -1 to use all runs if want to set trace output as well.
+     *
+     * @throws Exception
+     */
     public static void main(String args[]) throws Exception {
-        System.out.println("Hello, world!");
-        AI randAI = new RandomBiasedAI();
-        randAI.reset();
-        System.out.println("AI instantiated and reset");
+        String botfile = args[0];
+        String mapsfile = args[1];
+        String resultsfile = args[2];
+        int iterations = Integer.parseInt(args[3]);
 
-        AI stratAI = new StrategyTactics(utt);
-        stratAI.reset();
-        System.out.println("Created a third party AI");
+        loadBots(botfile, bots);
+        RunConfigurableExperiments.processBots(bots);
 
-        AI pvaiML_ED = new PVAIML_ED(utt);
-        pvaiML_ED.reset();
-        System.out.println("Created another third party AI");
+        loadMaps(mapsfile, maps);
+
+        PrintStream out = new PrintStream(new File(resultsfile));
+
+        int specificAI = -1;
+        if(args.length >= 5) {
+            specificAI = Integer.parseInt(args[4]);
+        }
+
+        String traceDir = null;
+        boolean saveTrace = false;
+        boolean saveZip = false;
+        if (args.length >= 6) {
+            saveTrace = true;
+            saveZip = true;
+            traceDir = args[5];
+        }
+
+        Experimenter.runExperiments(bots, maps, utt, iterations, 3000, 300, false, out,
+                specificAI, true, false, saveTrace, saveZip, traceDir);
+
     }
 }
